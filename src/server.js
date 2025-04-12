b399c91a2c6128fd08062008e6a50a8ed7d3989d
Emaus025 <remiileyva@gmail.com>
2025-04-12 16:56:49 -0600

    vercel2

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 // Configura CORS para permitir solicitudes desde el dominio de tu frontend
 // Modifica CORS para aceptar el dominio de Vercel
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://frontend-remedial.vercel.app'],
+  origin: ['http://localhost:3000', 'https://frontend-remedial-chi.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
